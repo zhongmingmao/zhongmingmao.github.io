5b93d5811a5c29c1959889db72a2fa605e158417
zhongmingmao <zhongmingmao0625@gmail.com>
2018-05-11 01:47:17 +0800

    完善脚本

--- build_gitbook.sh
+++ build_gitbook.sh
@@ -3,7 +3,7 @@ gitbook_path='source/gitbook/'
 rm -rf $gitbook_path
 mkdir -p $gitbook_path
 
-repository_names=('linear-algebra')
+repository_names[0]='linear-algebra'
 for repository_name in ${repository_names[*]}; do
 	rm -rf $repository_name
     repository='https://github.com/zhongmingmao/'$repository_name
